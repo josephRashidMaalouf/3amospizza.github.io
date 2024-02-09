44a78b2bc655992ce2a3cad99bc5363ac4764cc3
Joseph Rashid-Maalouf <139625730+josephRashidMaalouf@users.noreply.github.com>
2024-02-09 12:08:40 +0100

    .

--- scripts/shop.js
+++ scripts/shop.js
@@ -1,16 +1,12 @@
-const pizzas = [
-    {name: "Marghareta", price: 105, picURL: "./media/pizzas/marghareta.jpg", modalTag: "falafelPlateModal"},
-    {name: "Kebab pizza", price: 145, picURL: "./media/pizzas/kebabpizza.jpg", modalTag: "falafelPlateModal"},
-    {name: "Hawaii pizza", price: 145, picURL: "./media/pizzas/hawaii.jpg", modalTag: "falafelPlateModal"}
-];
+
 
 const plates = [
-    {name: "Falafeltallrik", price: 115, picURL: "./media/plates/falafel.jpg", modalTag: "falafelPlateModal"},
-    {name: "Kebabtallrik", price: 145, picURL: "./media/plates/kebabplate.jpg", modalTag: "falafelPlateModal"},
-    {name: "Shawarmatallrik", price: 155, picURL: "./media/plates/shawarma.jpg", modalTag: "falafelPlateModal"},
-    {name: "Marghareta", price: 105, picURL: "./media/pizzas/marghareta.jpg", modalTag: "falafelPlateModal"},
-    {name: "Kebab pizza", price: 145, picURL: "./media/pizzas/kebabpizza.jpg", modalTag: "falafelPlateModal"},
-    {name: "Hawaii pizza", price: 145, picURL: "./media/pizzas/hawaii.jpg", modalTag: "falafelPlateModal"}
+    {name: "Falafeltallrik", price: 115, picURL: "./media/plates/falafel.jpg", modalTag: "falafelPlateModal", description: "Krispig falafel med krispiga pommes, färsk sallad och vår hemlagade tarator."},
+    {name: "Kebabtallrik", price: 145, picURL: "./media/plates/kebabplate.jpg", modalTag: "kebabPlateModal", description: "Mört nötkött, krispiga pommes, fräsch sallad och valfri sås. En smakresa till traditionella smaker!"},
+    {name: "Shawarmatallrik", price: 155, picURL: "./media/plates/shawarma.jpg", modalTag: "ShawarmaPlateModal", description: "Traditionell shawarma med pommes, fräsch sallad och våra hemliga kryddor. En smakupplevelse utöver det vanliga!"},
+    {name: "Marghareta", price: 105, picURL: "./media/pizzas/marghareta.jpg", modalTag: "margharetaPizzaModal", description: " En klassisk Margherita pizza med mozzarella, tomater och basilika. En enkel men älskad favorit!"},
+    {name: "Kebab pizza", price: 145, picURL: "./media/pizzas/kebabpizza.jpg", modalTag: "KebabPizzaModal", description: "Mör kebab, smältande ost och krispiga grönsaker. En perfekt fusion av smaker på en pizza!"},
+    {name: "Hawaii pizza", price: 145, picURL: "./media/pizzas/hawaii.jpg", modalTag: "hawaiiPizzaModal", description: " En tropisk smakresa med ananas, skinka och ost. Söt och salt, perfekt balanserad!"}
 ];
 
 const cart = [];
@@ -68,14 +64,14 @@ function createCard(menuItem){
     readMoreBtn.classList.add("btn");
     readMoreBtn.classList.add("btn-primary");
     readMoreBtn.setAttribute('data-bs-toggle', 'modal');
-    readMoreBtn.setAttribute('data-bs-target', menuItem.modalTag);
-    readMoreBtn.textContent = 'More info';
+    readMoreBtn.setAttribute('data-bs-target', '#'+ menuItem.modalTag);
+    readMoreBtn.textContent = 'Mer info';
     menuCardBody.appendChild(readMoreBtn);
 
     const addToCartBtn = document.createElement("button");
     addToCartBtn.classList.add("btn");
     addToCartBtn.classList.add("btn-primary");
-    addToCartBtn.textContent = 'Add to cart';
+    addToCartBtn.textContent = 'Beställ';
     menuCardBody.appendChild(addToCartBtn);
 
     menuCard.appendChild(menuCardImg);
@@ -87,9 +83,84 @@ function createCard(menuItem){
 function createModal(menuItem){
     const modal = document.createElement("div");
     modal.id = menuItem.modalTag;
-    modal.classList.add("modal.dialog", "modal-dialog-centered")
+    modal.classList.add("modal", "fade");
 
-    
+    const modalDialog = document.createElement("div");
+    modalDialog.classList.add("modal-dialog", "modal-centered");
+
+    const modalContent = document.createElement("div");
+    modalContent.classList.add("modal-content");
+
+    //header
+    const modalHeader = document.createElement("div");
+    modalHeader.classList.add("modal-header");
+
+    const modalTitle = document.createElement('h5');
+    modalTitle.classList.add('modal-title');
+    modalTitle.textContent = `${menuItem.name} - ${menuItem.price} kr`;
+
+    const closeButton = document.createElement('button');
+    closeButton.setAttribute('type', 'button');
+    closeButton.classList.add('btn-close');
+    closeButton.setAttribute('data-bs-dismiss', 'modal');
+    closeButton.setAttribute('aria-label', 'Close');
+
+    modalHeader.appendChild(modalTitle);
+    modalHeader.appendChild(closeButton);
+
+    //body
+
+    const modalBody = document.createElement("div");
+    modalBody.classList.add("modal-body");
+
+    const grid = document.createElement("div");
+    grid.classList.add("modal-grid");
+    modalBody.appendChild(grid);
+
+    const img = document.createElement("img");
+    img.src = menuItem.picURL;
+
+    grid.appendChild(img);
+
+    const description = document.createElement("p");
+    description.innerText = menuItem.description;
+
+    grid.appendChild(description);
+
+
+
+
+    //footer
+    const modalFooter = document.createElement('div');
+    modalFooter.classList.add('modal-footer');
+
+
+    const goBackButton = document.createElement('button');
+    goBackButton.setAttribute('type', 'button');
+    goBackButton.classList.add('btn', 'btn-danger');
+    goBackButton.setAttribute('data-bs-dismiss', 'modal');
+    goBackButton.textContent = 'Tillbaka';
+
+
+    const addToCartBtn = document.createElement('button');
+    addToCartBtn.setAttribute('type', 'button');
+    addToCartBtn.classList.add('btn', 'btn-success');
+    addToCartBtn.textContent = 'Beställ';
+
+
+    modalFooter.appendChild(goBackButton);
+    modalFooter.appendChild(addToCartBtn);
+
+
+    //glue
+
+    modalContent.appendChild(modalHeader);
+    modalContent.appendChild(modalBody);
+    modalContent.appendChild(modalFooter);
+
+    modalDialog.appendChild(modalContent);
+
+    modal.appendChild(modalDialog);
 
     document.body.appendChild(modal);
 }
