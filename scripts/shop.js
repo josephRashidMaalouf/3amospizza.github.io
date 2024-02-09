3708953f74a484629fee5d5e3a77750856549123
Joseph Rashid-Maalouf <139625730+josephRashidMaalouf@users.noreply.github.com>
2024-02-09 10:28:01 +0100

    .

--- scripts/shop.js
+++ scripts/shop.js
@@ -1,13 +1,16 @@
 const pizzas = [
-    {name: "Marghareta", price: 105, picURL: "./media/pizzas/marghareta.jpg"},
-    {name: "Kebab pizza", price: 145, picURL: "./media/pizzas/kebabpizza.jpg"},
-    {name: "Hawaii pizza", price: 145, picURL: "./media/pizzas/hawaii.jpg"}
+    {name: "Marghareta", price: 105, picURL: "./media/pizzas/marghareta.jpg", modalTag: "falafelPlateModal"},
+    {name: "Kebab pizza", price: 145, picURL: "./media/pizzas/kebabpizza.jpg", modalTag: "falafelPlateModal"},
+    {name: "Hawaii pizza", price: 145, picURL: "./media/pizzas/hawaii.jpg", modalTag: "falafelPlateModal"}
 ];
 
 const plates = [
-    {name: "Falafeltallrik", price: 115, picURL: "./media/plates/falagel.jpg"},
-    {name: "Kebabtallrik", price: 145, picURL: "./media/plates/kebabplate.jpg"},
-    {name: "Shawarmatallrik", price: 155, picURL: "./media/pizzas/shawarma.jpg"}
+    {name: "Falafeltallrik", price: 115, picURL: "./media/plates/falafel.jpg", modalTag: "falafelPlateModal"},
+    {name: "Kebabtallrik", price: 145, picURL: "./media/plates/kebabplate.jpg", modalTag: "falafelPlateModal"},
+    {name: "Shawarmatallrik", price: 155, picURL: "./media/plates/shawarma.jpg", modalTag: "falafelPlateModal"},
+    {name: "Marghareta", price: 105, picURL: "./media/pizzas/marghareta.jpg", modalTag: "falafelPlateModal"},
+    {name: "Kebab pizza", price: 145, picURL: "./media/pizzas/kebabpizza.jpg", modalTag: "falafelPlateModal"},
+    {name: "Hawaii pizza", price: 145, picURL: "./media/pizzas/hawaii.jpg", modalTag: "falafelPlateModal"}
 ];
 
 const cart = [];
@@ -23,14 +26,70 @@ function removeFromCart(index){
     cart.splice(index, 1);
 }
 
-const menuTest = document.getElementById("menuTest");
+const plateMenus = document.getElementById("plateMenus");
+
 
 for(i = 0; i < plates.length; i++){
 
-    const menuItem = document.createElement("p");
-    
-    menuItem.innerHTML = `${plates[i].name}: ${plates[i].price} SEK`;
+    createCard(plates[i]);
 
-    menuTest.appendChild(menuItem);
+    //create modal
     
+    createModal(plates[i]);
+
+    
+
+    
+    
+}
+
+
+
+//functions
+
+function createCard(menuItem){
+    const menuCard = document.createElement("div");
+    menuCard.classList.add("card");
+
+
+    const menuCardImg = document.createElement("img");
+    menuCardImg.classList.add("card-img-top");
+    menuCardImg.src = menuItem.picURL;
+    menuCardImg.alt = "food image";
+
+    const menuCardBody = document.createElement("div");
+    menuCardBody.classList.add("card-body");
+
+    const foodTitle = document.createElement("h5");
+    foodTitle.innerText = menuItem.name;
+    menuCardBody.appendChild(foodTitle);
+
+    const readMoreBtn = document.createElement("button");
+    readMoreBtn.classList.add("btn");
+    readMoreBtn.classList.add("btn-primary");
+    readMoreBtn.setAttribute('data-bs-toggle', 'modal');
+    readMoreBtn.setAttribute('data-bs-target', menuItem.modalTag);
+    readMoreBtn.textContent = 'More info';
+    menuCardBody.appendChild(readMoreBtn);
+
+    const addToCartBtn = document.createElement("button");
+    addToCartBtn.classList.add("btn");
+    addToCartBtn.classList.add("btn-primary");
+    addToCartBtn.textContent = 'Add to cart';
+    menuCardBody.appendChild(addToCartBtn);
+
+    menuCard.appendChild(menuCardImg);
+    menuCard.appendChild(menuCardBody);
+
+    plateMenus.appendChild(menuCard);
+}
+
+function createModal(menuItem){
+    const modal = document.createElement("div");
+    modal.id = menuItem.modalTag;
+    modal.classList.add("modal.dialog", "modal-dialog-centered")
+
+    
+
+    document.body.appendChild(modal);
 }
